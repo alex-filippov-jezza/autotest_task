7bc74544d13e42d2549ab2c3d83f3fbf1b930f78
Alex Filippov <alex2000sti@gmail.com>
2024-04-18 20:23:54 +0300

    added url and title checking

--- tests/test_scenario2.py
+++ tests/test_scenario2.py
@@ -9,14 +9,15 @@ def test_scenario2(browser):
     browser.implicitly_wait(10)
     scenario2.open(sbis)
     scenario2.contacts_open()
-    region = scenario2.define_region()
+    region = scenario2.region_name
     assert region == 'Ярославская обл.', "Регион - не Ярославская область"
-    partners = scenario2.get_partners_list()
+    partners = scenario2.partners_list
     assert partners is not None, "Список партнеров есть"
     scenario2.change_region()
-    region_new = scenario2.define_region()
+    region_new = scenario2.region_name
     assert region_new != region, "Регион не изменился"
-    partners_new = scenario2.get_partners_list()
+    partners_new = scenario2.partners_list
     assert partners_new != partners, "Список партнеров не изменился"
+    assert scenario2.check_url_title(), "Заголовок и URL не соответствуют региону"
 
 
