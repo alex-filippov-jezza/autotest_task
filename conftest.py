11b21e7762b08859ee1aeb807dc0fcd96f3dd12b
Alex Filippov <alex2000sti@gmail.com>
2024-04-19 18:24:13 +0300

    added chrome options: disable safe downloads and changed download path

--- conftest.py
+++ conftest.py
@@ -1,9 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import pytest
 
 
 @pytest.fixture()
 def browser():
-    chrome_browser = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--safebrowsing-disable-download-protection")
+    prefs = {"download.default_directory": r"C:\Users\alex2\PycharmProjects\autotest_task\tests\\", "safebrowsing"
+                                                                                                    ".enabled": "true"}
+    chrome_options.add_experimental_option("prefs", prefs)
+
+    chrome_browser = webdriver.Chrome(chrome_options)
     chrome_browser.implicitly_wait(10)
+
     return chrome_browser
